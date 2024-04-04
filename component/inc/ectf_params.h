1744b9f57a4be4cbd5d1defed461b2a7c13fe5bd
rohanjha04 <rohan.dharmendra.jha@gmail.com>
2024-04-04 16:36:02 +0530

    trial by not encrypting data

--- component/inc/ectf_params.h
+++ component/inc/ectf_params.h
@@ -2,7 +2,7 @@
 #define __ECTF_PARAMS__
 #define COMPONENT_ID 286331173
 #define COMPONENT_BOOT_MSG "Component boot 2"
-#define ATTESTATION_LOC "2c41cb719e2de3397f0cb11ca871089e"
-#define ATTESTATION_DATE "d70cc06843818ff9df6ce15cafe6eb8c"
-#define ATTESTATION_CUSTOMER "b48f2349cd94ea94cc0dfe957b6ebeea"
+#define ATTESTATION_LOC "McLean 2"
+#define ATTESTATION_DATE "02/02/24"
+#define ATTESTATION_CUSTOMER "Jha 2"
 #endif
